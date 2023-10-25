a7a5f4047212f5cede767701dd6bbe7a94b4356b
hongjunyan <hongjunyan@moneydj.com>
2023-10-25 11:36:43 +0800

    fix bug in models.py

--- dockers/app/models.py
+++ dockers/app/models.py
@@ -35,29 +35,18 @@ class NERModel(object):
     def __init__(self, checkpoint: str, use_gpu=False):
         self.checkpoint = checkpoint
         self.use_gpu = use_gpu
-        self.pipeline = self.build_model()  # pull model from HF hub
-        # self.pipeline = self.build_model("./onnx")  # use local model
+        self.pipeline = self.build_model("./onnx")
 
-    def build_model(self, save_dir=None):
-        if save_dir is None:
-            if self.use_gpu:
-                model = ORTModelForTokenClassification.from_pretrained(self.checkpoint,
-                                                                       provider="CUDAExecutionProvider")
-            else:
-                model = ORTModelForTokenClassification.from_pretrained(self.checkpoint,
-                                                                       provider="CPUExecutionProvider")
-            tokenizer = AutoTokenizer.from_pretrained(self.checkpoint)
+    def build_model(self, save_dir: str):
+        save_dir = Path(save_dir)
+        if not save_dir.exists():
+            convert_hf_to_onnx(self.checkpoint, str(save_dir), pipeline_tag="token-classification")
+
+        if self.use_gpu:
+            model = ORTModelForTokenClassification.from_pretrained(str(save_dir), provider="CUDAExecutionProvider")
         else:
-            save_dir = Path(save_dir)
-            if not save_dir.exists():
-                convert_hf_to_onnx(self.checkpoint, str(save_dir), pipeline_tag="token-classification")
-
-            if self.use_gpu:
-                model = ORTModelForTokenClassification.from_pretrained(str(save_dir), provider="CUDAExecutionProvider")
-            else:
-                model = ORTModelForTokenClassification.from_pretrained(str(save_dir), provider="CPUExecutionProvider")
-            tokenizer = AutoTokenizer.from_pretrained(str(save_dir))
-
+            model = ORTModelForTokenClassification.from_pretrained(str(save_dir), provider="CPUExecutionProvider")
+        tokenizer = AutoTokenizer.from_pretrained(str(save_dir))
         return pipeline("token-classification", model=model, tokenizer=tokenizer, aggregation_strategy="simple")
 
     def __call__(self, inp_sentence: str):
